051de2e8136c0900b1eba7fa9ca2426c4a7127a5
Pratyush Gangwar <pratyush23395@iiitd.ac.in>
2024-08-27 09:22:19 +0530

    Renamed ehdrPtr to ehdr
    Added code to iterate over program header table.
    - Jump to ehdr.e_phoff via lseek() syscall
    - Use for-loop to iterate over the program headers at most ehdr.e_phnum times
    - Load the program header into memory via read(). Memory for the header is allocated via malloc
    - Check if header is of type PT_LOAD and whether ehdr.e_entry is contained in [phdr.p_vaddr, phdr.p_vaddr + phdr.memsz]

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -17,18 +17,30 @@ void loader_cleanup() {
 void load_and_run_elf(char** exe) {
   fd = open(exe[1], O_RDONLY);
   // 1. Load entire binary content into the memory from the ELF file.
-  Elf32_Ehdr* ehdrPtr = malloc( sizeof(Elf32_Ehdr) );
-  read(fd, ehdrPtr, sizeof(Elf32_Ehdr));
+  ehdr = malloc( sizeof(Elf32_Ehdr) );
+  read(fd, ehdr, sizeof(Elf32_Ehdr));
 
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
   //    type that contains the address of the entrypoint method in fib.c
+ 
+  lseek(fd, ehdr->e_phoff, SEEK_SET);
+
+  for(int i = 0; i < ehdr->e_phnum; i++) {
+    phdr = malloc( sizeof(Elf32_Phdr) );
+    read(fd, phdr, sizeof(Elf32_Phdr));
+    
+    if (phdr->p_type == PT_LOAD && phdr->p_vaddr <= ehdr->e_entry && ehdr->e_entry < phdr->p_vaddr + phdr->p_memsz) {
+      break;
+    } 
+  }
+
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
-  int result = _start();
-  printf("User _start return value = %d\n",result);
+  // int result = _start();
+  // printf("User _start return value = %d\n",result);
 }
 
 int main(int argc, char** argv) 

47f39c62fc52c6888fd5b388be09c18e78c4307f
Pratyush Gangwar <pratyush23395@iiitd.ac.in>
2024-08-27 12:16:37 +0530

    Added error handling
    - open() returns -1 if file can't be opened
    - read() returns -1 if file can't be read
    - lseek() returns -1 if file can't be seeked
    - malloc() returns NULL if memory can't be allocated
    - mmap() returns MAP_FAILED if memory can't be allocated
    
    - TODO: implement validation of input file (i.e, is input file actually 32-bit ELF)

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -4,6 +4,7 @@ Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 char* virtual_mem;
 int fd;
+int ret;
 
 /*
  * release memory and other cleanups
@@ -25,32 +26,79 @@ void loader_cleanup() {
  */
 void load_and_run_elf(char** exe) {
   fd = open(exe[1], O_RDONLY);
+  if (fd == 1) {
+    printf("Error while opening file.");
+    exit(1);
+  }
 
   // 1. Load entire binary content into the memory from the ELF file.
   ehdr = malloc( sizeof(Elf32_Ehdr) );
-  read(fd, ehdr, sizeof(Elf32_Ehdr));
+  if (ehdr == NULL) { 
+    printf("Error while malloc'ing Ehdr");
+    exit(1);
+  }
+
+  ret = read(fd, ehdr, sizeof(Elf32_Ehdr));
+  if (ret == -1) {
+    printf("Error while reading Ehdr");
+    exit(1);
+  }
 
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
   //    type that contains the address of the entrypoint method in fib.c
-  lseek(fd, ehdr->e_phoff, SEEK_SET);
+  ret = lseek(fd, ehdr->e_phoff, SEEK_SET);
+  if (ret == (off_t) - 1) {
+    printf("Error while seeking to program header table.");
+    exit(1);
+  }
+
+  int entry_found = 0;
 
   for(int i = 0; i < ehdr->e_phnum; i++) {
     phdr = malloc( sizeof(Elf32_Phdr) );
-    read(fd, phdr, sizeof(Elf32_Phdr));
+    if (phdr == NULL) { 
+      printf("Error while malloc'ing Phdr at index %d", i);
+      exit(1);
+    }
+
+    ret = read(fd, phdr, sizeof(Elf32_Phdr));
+    if (ret == -1) {
+      printf("Error while reading Ehdr at index %d", i);
+      exit(1);
+    }
     
     if (phdr->p_type == PT_LOAD && phdr->p_vaddr <= ehdr->e_entry && ehdr->e_entry < phdr->p_vaddr + phdr->p_memsz) {
+      entry_found = 1;
       break;
     } 
   } 
+
+  if (!entry_found) {
+    printf("Entry point doesn't exist.");
+    exit(1);
+  }
 
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
 
   // mmap returns void* but pointer arithmetic gives error with void*
   virtual_mem = mmap(NULL, phdr->p_memsz, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, 0, 0);
+  if (virtual_mem == MAP_FAILED) {
+    printf("Failed to allocate virtual memory");
+    exit(1);
+  }
 
-  lseek(fd, phdr->p_offset, SEEK_SET);
-  read(fd, virtual_mem, phdr->p_filesz );
+  ret = lseek(fd, phdr->p_offset, SEEK_SET);
+  if (ret == -1) {
+    printf("Error while seeking to the executable segment");
+    exit(1);
+  }
+
+  ret = read(fd, virtual_mem, phdr->p_filesz );
+  if (ret == -1) {
+    printf("Error while writing bytes from file to virtual memory");
+    exit(1);
+  }
 
   // Remaining bytes must be set to 0
   if (phdr->p_memsz > phdr->p_filesz) {
